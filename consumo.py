799d58c2e0f52a78e541e596d12fe0838a1a431f
Pedro Eli Bernardes Maciel <149798891+pedroeli07@users.noreply.github.com>
2024-06-13 13:13:49 -0300

    Update consumo.py

--- consumo.py
+++ consumo.py
@@ -1,9 +1,25 @@
-def calculate_consumption_generation(df_copy):
-    # Agrega os dados mensais somando os consumos e somente as gerações não nulas
-    monthly_data = df_copy.groupby('Período').agg({
-        'Consumo': 'sum', 
-        'Geração': lambda x: x[x != 0].sum()
-    })
+def calculate_consumption_generation(df_copy, calc_type="Geração"):
+    if calc_type == "Geração":
+        aggregation = {
+            'Consumo': 'sum', 
+            'Geração': lambda x: x[x != 0].sum()
+        }
+        columns_rename = {
+            'Consumo': 'Consumo [kWh]', 
+            'Geração': 'Energia Injetada [kWh]'
+        }
+    elif calc_type == "Compensação":
+        aggregation = {
+            'Consumo': 'sum', 
+            'Compensação': 'sum'
+        }
+        columns_rename = {
+            'Consumo': 'Consumo [kWh]', 
+            'Compensação': 'Compensação [kWh]'
+        }
+    
+    # Agrega os dados mensais somando os consumos e somente as gerações/compensações não nulas
+    monthly_data = df_copy.groupby('Período').agg(aggregation)
     
     # Mapeamento de números de meses para abreviações em português
     months_map = {
@@ -17,16 +33,20 @@ def calculate_consumption_generation(df_copy):
         lambda x: f"{months_map[x.split('/')[0]]}/{x.split('/')[1]}"
     )
     
-    # Renomeia as colunas para inclusão das unidades
-    monthly_data = monthly_data.rename(columns={
-        'Consumo': 'Consumo [kWh]', 
-        'Geração': 'Energia Injetada [kWh]'
-    })
+    # Arredondar os valores para uma casa decimal
+    monthly_data = monthly_data.round(1)
     
-    # Calcula a média do consumo e geração e adiciona ao dataframe como uma nova linha 'Média'
+    # Renomeia as colunas para inclusão das unidades
+    monthly_data = monthly_data.rename(columns=columns_rename)
+    
+    # Calcula a média do consumo e geração/compensação e adiciona ao dataframe como uma nova linha 'Média'
     average_consumption = monthly_data['Consumo [kWh]'].mean()
-    average_generation = monthly_data['Energia Injetada [kWh]'].mean()
-    monthly_data.loc['Média'] = [int(average_consumption), int(average_generation)]
+    if calc_type == "Geração":
+        average_generation = monthly_data['Energia Injetada [kWh]'].mean()
+        monthly_data.loc['Média'] = [int(average_consumption), int(average_generation)]
+    elif calc_type == "Compensação":
+        average_compensacao = monthly_data['Compensação [kWh]'].mean()
+        monthly_data.loc['Média'] = [int(average_consumption), int(average_compensacao)]
     
     # Reseta o índice para transformar os períodos em uma coluna novamente
     monthly_data.reset_index(inplace=True)
